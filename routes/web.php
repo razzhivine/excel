67e0fa5570ed5357ab414230beea6254ce19418b
Eugene <razzhivine@gmail.com>
2025-02-03 13:59:56 +0400

    Removing everything related to "Projects"

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,12 @@
 
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -25,10 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/projects', [ProjectController::class, 'index'])->name('project.index');
-    Route::get('/projects/import', [ProjectController::class, 'import'])->name('project.import');
-    Route::post('/projects/import', [ProjectController::class, 'store'])->name('project.import.store');
 
     Route::get('/tasks', [TaskController::class, 'index'])->name('task.index');
     Route::get('/tasks/{task}/failed_list', [TaskController::class, 'failedList'])->name('task.failed_list');
